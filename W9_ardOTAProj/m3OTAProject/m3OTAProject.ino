e394b804baeedcd692757834f69f475a87ccefde
dadalee51 <dadalee51@gmail.com>
2024-05-22 12:42:36 +1000

    update comment

--- W9_ardOTAProj/m3OTAProject/m3OTAProject.ino
+++ W9_ardOTAProj/m3OTAProject/m3OTAProject.ino
@@ -1,31 +1,6 @@
 /**
-  The plan is to use esp m3 as a master to control the
-  two ATtiny 1616 mcu, where:
-  at1 slave address: 0x12 (decimal value 18)
-  at2 slave address 0x14 (decimal value 20)
-  acccelerometer address is 0x19 LIS2DH12TR
-  multiplexor 0x70
-  color sensor: 0x10
-  AT1 -
-      Motor A/B
-      RLED1, WLED1
-      ENCODER_A/B/C
-      TXD2/RXD2 (connected to m3 but no use) - this is improved by 2.25b
-      RGB led.
-  AT2 -
-      IRSensor SEN1/2/3 (sensor 1 and 3 requires IR1/ Pin(16) to turn on led.
-      LDR - SEN13/14
-      Motor C/D
-      RLED1, WLED1
-      encoder:C/D
-      Multiplexor
-  i2c address:
-      Decimal address:  16  | Hexa address:  0x10 - color sensors
-      Decimal address:  18  | Hexa address:  0x12 -AT1
-      Decimal address:  20  | Hexa address:  0x14 -AT2
-      Decimal address:  25  | Hexa address:  0x19 -accelerotmeter
-      Decimal address:  41  | Hexa address:  0x29 - tof
-      Decimal address:  112  | Hexa address:  0x70 - multiplexor
+  this is a unbricking first aid project to open in arduino.
+  NOTE: this file is not to be used in vscode.
   Software copyright 2024, tigo.robotics
  */
 #include <ArduinoOTA.h>
@@ -75,91 +50,11 @@ void setup() {
   pinMode(2, OUTPUT);
   Wire.setClock(400000); //set i2c fast mode.
   Wire.begin(4,14); //join i2c as master //sda4,scl14
-
-#ifdef TF_ON
-  if (!sensor.init()) {
-    FOR(k,3){
-    signalling(30);
-    delay(1000);
-    }
-  }
-  sensor.setTimeout(500);
-  sensor.startContinuous();
-  #endif
-  
-  #ifdef ACC_ON
-  if (!accel.begin()) {
-    signalling(30);
-    delay(100);
-  }
-  #endif
-  #ifdef CLR_ON
-    if(!RGBWSensor.begin()) {
-      FOR(i,5){
-        signalling(30);
-        delay(200);
-      }
-    }
-  #endif
-
-  #ifdef CLR_ON
-  //switch to first clr sensr
-  TCA9548A(0);
-  RGBWSensor.setConfiguration(VEML6040_IT_40MS + VEML6040_AF_AUTO + VEML6040_SD_ENABLE);
-  delay(500);
-  FOR(i,5){
-    if(!RGBWSensor.begin()) {
-      signalling(30);
-      delay(1000);
-    }
-  }
-  //switch to first clr sensr
-  TCA9548A(1);
-  RGBWSensor.setConfiguration(VEML6040_IT_40MS + VEML6040_AF_AUTO + VEML6040_SD_ENABLE);
-  delay(500);
-  FOR(i,5){
-   if(!RGBWSensor.begin()) {
-     signalling(30);
-     delay(1000);
-    }
-  }
-  #endif  
 }
 
 void loop() {
   ArduinoOTA.handle();
   digitalWrite(2,1);
-  #ifdef TF_ON
-  head=sensor.readRangeContinuousMillimeters();
-  if (sensor.timeoutOccurred()) FOR(k,3)signalling(50);
-  if(head > 300){
-    digitalWrite(WLED2,1); //turn on
-    to_WLED1('A');
-  }else{
-    digitalWrite(WLED2,0);
-    to_WLED1('B');
-  }
-  //to_Int(head);
-  #endif
-
-
-  #ifdef ACC_ON
-  z_acc = accel.getZ();
-  if (z_acc < 0){
-    digitalWrite(WLED1, 1); //on
-  }else{
-    digitalWrite(WLED1, 0);//Wled off
-  }
-  #endif
-  
- #ifdef CLR_ON
-  TCA9548A(0);
-  r1 = RGBWSensor.getRed();
-  g1 = RGBWSensor.getGreen();
-  b1 = RGBWSensor.getBlue();
-  w1 = RGBWSensor.getWhite();
-  
- #endif 
 }
 
 
